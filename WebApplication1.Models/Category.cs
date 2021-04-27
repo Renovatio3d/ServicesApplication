928554bac4e571643a2373e68acb308ecdb27840
Jefferson Nah <renovatio3d@gmail.com>
2021-04-26 20:20:02 -0400

    Category model added to webApp model!

---
+++ WebApplication1.Models/Category.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    class Category
+    {
+        //data annotation call key
+        [Key]
+
+        // prop id 
+        public int Id { get; set; }
+
+        // few more data annotations, Required prop which will fall back to the Key notation, and Display prop which is the view of our "string Name"
+        [Required]
+        [Display(Name="Category Name")]
+
+        // next we will have a "name" and "display order"
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Display Order")]
+        public int DisplayOrder { get; set; }
+
+
+
+    }
+}
