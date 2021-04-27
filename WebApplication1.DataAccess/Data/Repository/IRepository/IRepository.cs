a8af06f7adc6a2d6e6216d0f981b7a4290679898
Jefferson Nah <renovatio3d@gmail.com>
2021-04-26 21:54:25 -0400

    Implement repository, and irepository pattern

---
+++ WebApplication1.DataAccess/Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.DataAccess.Data.Repository.IRepository
+{
+    // Use for category or any class, "T" implemente, or accept any object. 
+    public interface IRepository<T> where T: class
+    {
+        // basic get method, a class that return a generic T or class 
+        T Get(int id);
+
+        // Retrieve a list of category by GetAll, with a return Type of IEnumerable
+        // GetAll a certain Parameter, filter data, orderBy, and properties
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+        // Return of one object from filiter 
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter = null,
+            string includeProperties = null
+            );
+        // Add or Remove any entity from database
+        void Add(T entity);
+
+        void Remove(int id);
+        void Remove(T entity);
+    }
+}
---
+++ WebApplication1.DataAccess/Data/Repository/Repository.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.DataAccess.Data.Repository.IRepository;
+
+namespace WebApplication1.DataAccess.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        // Access database with use of the DBContexts class, always.
+        protected readonly DbContext Context;
+        internal DbSet<T> dbSet;
+
+        // Initialize Contexts with us of dependency injection 
+        public Repository (DbContext context)
+        {
+            Context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+
+        public void Add(T entity)
+        {
+            // With entity add to existing dbSet
+            dbSet.Add(entity);
+        }
+
+        public T Get(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if(filter != null)
+            {
+                // Append the filter on the query, and implement all filter 
+                query = query.Where(filter);
+            }
+
+            //Include properties will be comma seperated, check for include properties
+            if(includeProperties != null)
+            {
+                foreach(var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    // Each include and property will then add to query
+                    query = query.Include(includeProperty);
+                }
+
+            }
+  
+            if(orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            return query.ToList();
+        
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                // Append the filter on the query, and implement all filter 
+                query = query.Where(filter);
+            }
+
+            //Include properties will be comma seperated, check for include properties
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    // Each include and property will then add to query
+                    query = query.Include(includeProperty);
+                }
+
+            }
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(int id)
+        {
+            T entityToRemove = dbSet.Find(id);
+            Remove(entityToRemove);
+
+        }
+
+        public void Remove(T entity)
+        {
+          
+            dbSet.Remove(entity);
+        }
+    }
+}
